c8ed25bd7c056abfef95d5a23c420f1f043b6cd8
mneelam2 <mneelam@my.bridgeport.edu>
2023-12-06 00:37:18 -0500

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Welcome to Cloud Computing Course'));
+app.get('/', (req, res) => res.send('Hello Welcome to Univerisity of Bridgeport'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
